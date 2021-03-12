cf0bf18ae78dfcd6f800d0e35dbcd59dba73ac69
Matti Jukola <buq2@buq2.com>
2021-03-12 19:29:06 +0100

    flake8 cleanup

--- my_network.py
+++ my_network.py
@@ -1,4 +1,4 @@
-#/bin/python3
+#!/bin/python3
 import shelve
 import datetime
 import bisect
@@ -6,16 +6,17 @@ import functools
 import logging
 import copy
 
-DB_FILENAME_DEV='network_stats.slv'
-DB_FILENAME='/home/pi/'+DB_FILENAME_DEV
-RECEIVED='received'
-TRANSMITTED='transmitted'
-TIMESERIES_NAME='timeseries'
+DB_FILENAME_DEV = 'network_stats.slv'
+DB_FILENAME = '/home/pi/' + DB_FILENAME_DEV
+RECEIVED = 'received'
+TRANSMITTED = 'transmitted'
+TIMESERIES_NAME = 'timeseries'
 MAX_TIMESERIESPOINT_LIFE = datetime.timedelta(days=2)
 MIN_DIFFERENCE_BETWEEN_TIMESERIES_POINTS = datetime.timedelta(seconds=50)
-FORCE_MONOTONIC_TIMESERIES=True
+FORCE_MONOTONIC_TIMESERIES = True
 
 log = logging.getLogger(__name__)
+
 
 def __get_network_interface_state():
     out = {}
@@ -25,12 +26,14 @@ def __get_network_interface_state():
             interface = interface.strip()
             data = rest.split()
             if len(data) >= 16:
-                out[interface] = {RECEIVED:int(data[0]), TRANSMITTED:int(data[8])}
-        except:
+                out[interface] = {RECEIVED: int(
+                    data[0]), TRANSMITTED: int(data[8])}
+        except BaseException:
             # Ignore header and incorrectly formatted data
             pass
 
     return out
+
 
 @functools.total_ordering
 class TimeseriesPoint:
@@ -73,14 +76,13 @@ class TimeseriesPoint:
             return out
 
         if isinstance(out, dict):
-            for key,val in out.items():
+            for key, val in out.items():
                 out[key] += self.__monotonic_fix.get(key, 0)
         else:
             out += self.__monotonic_fix
 
         return out
 
-    
     def __lt__(self, other):
         if isinstance(other, TimeseriesPoint):
             return self.datetime < other.datetime
@@ -97,7 +99,9 @@ class Timeseries:
         if time is None:
             time = datetime.datetime.now()
 
-        if len(self) > 0 and time - self.get_latest().datetime < MIN_DIFFERENCE_BETWEEN_TIMESERIES_POINTS:
+        if len(self) > 0 and time - \
+                self.get_latest().datetime < \
+                MIN_DIFFERENCE_BETWEEN_TIMESERIES_POINTS:
             log.info('Previous timepoint too close, ignoring new point')
             return
 
@@ -110,8 +114,9 @@ class Timeseries:
 
     def __check_monotonicness(self, point):
         # Check for monotonicness
-        # This is needed as autopi modem resets transferred bytes each time autopi sleeps
-        # so if we see that previous values are lower than current ones, we need to fix them
+        # This is needed as autopi modem resets transferred bytes each
+        # time autopi sleeps so if we see that previous values are
+        # lower than current ones, we need to fix them
 
         if len(self) == 0:
             # No data, can not yet fix issues (/there is no issues)
@@ -142,19 +147,19 @@ class Timeseries:
         if idx == 0:
             return self.__data[idx], 0
         elif idx == len(self.__data):
-            return self.__data[-1], len(self.__data)-1
-        before = self.__data[idx-1]
+            return self.__data[-1], len(self.__data) - 1
+        before = self.__data[idx - 1]
         after = self.__data[idx]
         if after.datetime - time < time - before.datetime:
             return after, idx
         else:
-            return before, idx-1
+            return before, idx - 1
 
     def prune(self):
         limit_datetime = datetime.datetime.now() - MAX_TIMESERIESPOINT_LIFE
         (point, idx) = self.__get_closest_to_time(limit_datetime)
         if point.datetime < limit_datetime:
-            del self.__data[:idx+1]
+            del self.__data[:idx + 1]
         else:
             del self.__data[:idx]
 
@@ -166,10 +171,11 @@ class Timeseries:
 
     def sort(self):
         if len(self) > 0:
-            self.__data.sort(key = lambda x : x.datetime)
+            self.__data.sort(key=lambda x: x.datetime)
         self.__sorted = True
 
-def __calculate_network_stats(ts, delta):
+
+def __calculate_network_stats(ts, delta, weighted=False):
     out = {}
     latest = ts.get_latest()
     prev = ts.get_closest_to_time(latest.datetime - delta)
@@ -177,26 +183,43 @@ def __calculate_network_stats(ts, delta):
 
     data_latest = latest.get_data()
     data_prev = prev.get_data()
+
+    # If requested interval was 1day, but for what ever reason we have only
+    # 12 hours of data, to get average consuptions with current rate,
+    # we should multiply the consumption in 12 hours by 2.
+    if weighted and real_delta.total_seconds() != 0:
+        multiplier = delta.total_seconds() / real_delta.total_seconds()
+    else:
+        # Probably in a test which data is created almost instantly
+        multiplier = 1
+
     for key, val_latest in data_latest.items():
         if key not in data_prev:
             continue
-        out[key] = val_latest - data_prev[key]
+        out[key] = (val_latest - data_prev[key]) * multiplier
     return out
 
-def __process_data(network_timeseries, interface = None, delta = datetime.timedelta(days=1)):
+
+def __process_data(
+    network_timeseries,
+    interface=None,
+    delta=datetime.timedelta(
+        days=1)):
     out = {}
-    
+
     if interface is not None:
-        out[interface] = __calculate_network_stats(network_timeseries[interface], delta)
+        out[interface] = __calculate_network_stats(
+            network_timeseries[interface], delta)
     else:
         for interface, ts in network_timeseries.items():
             out[interface] = __calculate_network_stats(ts, delta)
-            
+
     return out
+
 
 def __update_network_timeseries(timeseries):
     state = __get_network_interface_state()
-    for key,val in state.items():
+    for key, val in state.items():
         if key not in timeseries:
             ts = Timeseries()
             timeseries[key] = ts
@@ -204,12 +227,13 @@ def __update_network_timeseries(timeseries):
             ts = timeseries[key]
         ts.add(val)
 
-    for key,ts in timeseries.items():
+    for key, ts in timeseries.items():
         ts.prune()
 
         # Remove timeseries which have no data
         if len(ts) == 0:
             del timeseries[key]
+
 
 def clear():
     """
@@ -218,9 +242,10 @@ def clear():
 
     try:
         db = shelve.open(DB_FILENAME)
-    except:
+    except BaseException:
         db = shelve.open(DB_FILENAME_DEV)
     db.clear()
+
 
 def get_network_usage():
     """
@@ -231,7 +256,7 @@ def get_network_usage():
     # Get history
     try:
         db = shelve.open(DB_FILENAME)
-    except:
+    except BaseException:
         db = shelve.open(DB_FILENAME_DEV)
 
     if TIMESERIES_NAME in db:
@@ -253,7 +278,8 @@ def help():
     Shows this help information.
     """
 
-    return __salt__["sys.doc"]("my_network")
+    return __salt__["sys.doc"]("my_network")  # noqa: F821
+
 
 if __name__ == '__main__':
     print(get_network_usage())
--- test_my_network.py
+++ test_my_network.py
@@ -3,6 +3,7 @@ import pytest
 import datetime
 import numpy as np
 
+
 class FakedNet:
     def __init__(self):
         self.received = 0
@@ -15,13 +16,14 @@ class FakedNet:
 
     def get_fake_network_interface_state(self):
         out = {}
-        out[self.name] = {my_network.RECEIVED:self.received, my_network.TRANSMITTED:self.transmitted}
+        out[self.name] = {my_network.RECEIVED: self.received,
+                          my_network.TRANSMITTED: self.transmitted}
 
         return out
 
     def increase_net_usage(self):
-        rec = np.random.randint(1,10)
-        trans = np.random.randint(1,10)
+        rec = np.random.randint(1, 10)
+        trans = np.random.randint(1, 10)
         self.received += rec
         self.transmitted += trans
 
@@ -30,33 +32,41 @@ class FakedNet:
 
     def reset(self):
         # Set non totals to smaller than previously
-        self.received = np.min([np.random.randint(0,10),self.received-1])
-        self.transmitted = np.min([np.random.randint(0,10), self.transmitted-1])
+        self.received = np.min([np.random.randint(0, 10), self.received - 1])
+        self.transmitted = np.min(
+            [np.random.randint(0, 10), self.transmitted - 1])
 
         self.total_received += self.received
         self.total_transmitted += self.transmitted
 
+
 @pytest.fixture
 def net():
-    my_network.MIN_DIFFERENCE_BETWEEN_TIMESERIES_POINTS = datetime.timedelta(seconds=0)
+    my_network.MIN_DIFFERENCE_BETWEEN_TIMESERIES_POINTS = datetime.timedelta(
+        seconds=0)
     my_network.clear()
     net = FakedNet()
-    my_network.__get_network_interface_state = lambda: net.get_fake_network_interface_state()
+    my_network.__get_network_interface_state = \
+        lambda: net.get_fake_network_interface_state()
     return net
+
 
 def check(net, state):
     assert state[net.name][my_network.RECEIVED] == net.total_received
     assert state[net.name][my_network.TRANSMITTED] == net.total_transmitted
 
+
 def test_init(net):
     assert net.received == 0
     assert net.transmitted == 0
+
 
 def test_without_reset(net):
     for i in range(5):
         state = my_network.get_network_usage()
         check(net, state)
         net.increase_net_usage()
+
 
 def test_with_reset(net):
     for i in range(5):
@@ -68,6 +78,7 @@ def test_with_reset(net):
     net.reset()
     state = my_network.get_network_usage()
     check(net, state)
+
 
 def test_with_reset2(net):
     for i in range(5):
@@ -83,6 +94,7 @@ def test_with_reset2(net):
         state = my_network.get_network_usage()
         check(net, state)
         net.increase_net_usage()
+
 
 def test_with_reset3(net):
     for i in range(5):
@@ -107,6 +119,7 @@ def test_with_reset3(net):
         state = my_network.get_network_usage()
         check(net, state)
         net.increase_net_usage()
+
 
 if __name__ == '__main__':
     pytest.main()
